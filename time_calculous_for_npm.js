d14e316d197da5a5365885999c37f7d010e94245
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-01-17 08:22:12 +0100

    Adding arguments at the first function

--- time_calculous_for_npm.js
+++ time_calculous_for_npm.js
@@ -1,3 +1,8 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
+function wished_wday_in_choosen_month(year, month, wday, hour, minute, 
+second) {
+
+}
+
+module.exports = {
+	'wished_wday_in_choosen_month': wished_wday_in_choosen_month
 }
