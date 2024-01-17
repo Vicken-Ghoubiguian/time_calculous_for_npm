10cd8dd20bc6a5507f0dafbd6d616dbedd45166d
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-01-17 10:28:11 +0100

    Coming soon...

--- time_calculous_for_npm.js
+++ time_calculous_for_npm.js
@@ -2,9 +2,17 @@
 function wished_wday_in_choosen_month(year, month, wday, hour, minute, 
 second) {
 
+	console.log("wished_wday_in_choosen_month coming soon...");
+}
+
+//
+function calculations_on_date_and_time() {
+
+	console.log("calculations_on_date_and_time coming soon...");
 }
 
 //
 module.exports = {
+	'calculations_on_date_and_time': calculations_on_date_and_time,
 	'wished_wday_in_choosen_month': wished_wday_in_choosen_month
 }
